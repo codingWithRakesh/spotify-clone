b6d9f251549e00c98ff7fd87bfd33f8cb0290928
Tarapada Garai <146563644+codingWithRakesh@users.noreply.github.com>
2025-06-21 04:16:15 +0530

    refactor: update greeting message in HomePage to be dynamic based on the current time

--- frontend/src/pages/home/HomePage.tsx
+++ frontend/src/pages/home/HomePage.tsx
@@ -19,6 +19,20 @@ const HomePage = () => {
 
 	const { initializeQueue } = usePlayerStore();
 
+	const getGreeting = () => {
+		const currentHour = new Date().getHours();
+
+		if (currentHour < 12) {
+			return "Good Morning";
+		} else if (currentHour < 17) {
+			return "Good Afternoon";
+		} else if (currentHour < 20) {
+			return "Good Evening";
+		} else {
+			return "Good Night";
+		}
+	};
+
 	useEffect(() => {
 		fetchFeaturedSongs();
 		fetchMadeForYouSongs();
@@ -37,7 +51,7 @@ const HomePage = () => {
 			<Topbar />
 			<ScrollArea className='h-[calc(100vh-180px)]'>
 				<div className='p-4 sm:p-6'>
-					<h1 className='text-2xl sm:text-3xl font-bold mb-6'>Good afternoon</h1>
+					<h1 className='text-2xl sm:text-3xl font-bold mb-6'>{getGreeting()}</h1>
 					<FeaturedSection />
 
 					<div className='space-y-8'>
